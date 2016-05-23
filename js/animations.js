cffa71e776e037aa2fc0ae6287a47ce92142d29e
Scott Hendrickson <Scott Hendrickson>
2016-05-23 10:13:02 -0600

    Not complete but working

--- js/animations.js
+++ js/animations.js
@@ -1,11 +1,26 @@
 $(document).ready(function() {
 //Initially, the Tweet button and the character count button should be hidden (CSS).
+//Recommendation is to save all your paths as variables for easy use later.
 (function(){
   $('#char-count').hide();
   $('#tweet-submit').hide();
   $('.tweet-actions').hide();
   $('.stats').hide();
 })();
+
+var storage = [];
+var userName = '@bearabon';
+var fullName = "Scott Hendrickson";
+var avatarLocation = "img/volibear.jpg";
+var Tweet = function(dateStamp, userName, fullName, avatarLocation, message) {
+  this.dateStamp = dateStamp;
+  this.favoritedCount = 0;
+  this.retweetedCount = 0;
+  this.userName = userName;
+  this.fullName = fullName;
+  this.avatarLocation = avatarLocation;
+  this.message = message;
+}
 
 //When the user clicks on the textarea, the textarea should double in size
 // and the character count and Tweet buttons should be revealed.
@@ -56,22 +71,34 @@ $(document).ready(function() {
     };
   });
 
+
 /*When the user successfully inputs characters and clicks the “Tweet” button, a
 new tweet should be created and added to the tweet stream in the main column,
 using the user’s fake profile image in the top left and username/fullname.*/
 var text ='';
 var addTweet = function(text) {
-
   if(text.length > 0 && text.length <= 140) {
+/*   var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    var date = new Date();
+    var year = date.getFullYear();
+    var month = date.getMonth();
+    for (var i = 0; i < monthNames.length; i++) {
+      if (i === month) {
+        month = monthNames[i];
+      }
+    };
+    var day = date.getDate();
+    var dateStamp = month + " " + day + ", " + year;  */
 
+    var dateStamp = jQuery.timeago(new Date());
     $('#stream').prepend(
       '<div class="tweet">' +
       '<div class="content">' +
 
     //should fix this block to use the persons actual account info rather than hard coded information
-        '<img class="avatar" src="img/alagoon.jpg" />' +
-        '<strong class="fullname">Your Name Here</strong>' +
-        '<span class="username">@Your Name Here</span>' +
+        '<img class="avatar" src="' + avatarLocation + '" />' +
+        '<strong class="fullname">' + fullName +  '</strong>' +
+        '<span class="username">' + userName + '</span>' +
 
             '<p class="tweet-text">' + text + '</p>' +
 
@@ -96,32 +123,62 @@ var addTweet = function(text) {
           '<div class="users-interact">' +
             '<div>' +
 
-              '<img src="img/alagoon.jpg" />' +
+              '<img src="' + avatarLocation + '" />' +
               '<img src="img/vklimenko.jpg" />' +
             '</div>' +
           '</div>' +
 
         '<div class="time">' +
-            ':04 PM - 19 Sep 13' +
+            '<time class="timeago" datetime="2008-07-17T09:24:17Z">' + dateStamp + '</time>' +
           '</div>' +
         '</div>' +
         '<div class="reply">' +
-          '<img class="avatar" src="img/alagoon.jpg" />' +
-          '<textarea class="tweet-compose" placeholder="Reply to @mybff"/></textarea>' +
+          '<img class="avatar" src="' + avatarLocation + '" />' +
+          '<textarea class="tweet-compose" placeholder="Reply to '+ userName + '"/></textarea>' +
         '</div>' +
       '</div>' +
-    '</div>'
+    '</div><!-- .tweet -->'
     );
   } else if ( text.length > 140){
     alert('Your text must have fewer than 140 characters. Currently you are at: ' + text.length);
   } else if (text.length === 0) {
     alert('Please add text to your tweet');
   }
+  (function(){
+    $('#char-count').hide();
+    $('#tweet-submit').hide();
+    $('.tweet-actions').hide();
+    $('.stats').hide();
+  })();
+  $('.tweet').mouseenter(function() {
+    $(this).find('.tweet-actions').fadeIn('fast');
+    });
+  $('.tweet').mouseleave(function() {
+    $(this).find('.tweet-actions').fadeOut('fast');
+  });
+  $('.tweet-compose').focus(function() {
+       $(this).animate({
+           height: '5em'
+       });
+       //$('#char-count').fadeTo('fast', 1);
+       //$('#tweet-submit').fadeTo('fast', 1);
+   });
+   $('.tweet-compose').focusout(function() {
+        $(this).animate({
+            height: '2.5em'
+        });
+    });
+    $('.tweet').click(function() {
+      $(this).find('.stats').show('fast');
+    });
+    $('.tweet').mouseleave(function() {
+      $(this).find('.stats').hide('fast');
+    });
 };
 
 
 
-      $('#tweet-submit').click( function(e) {
+      $('#tweet-submit').on('click', function(e) {
         e.preventDefault();
         text = $('.tweet-compose').val();
         addTweet(text);
@@ -160,8 +217,11 @@ $('.tweet').mouseleave(function() {
 
 //## Black Diamond
 var storage = [];
-var Tweet = function(userName, fullName, avatarLocation, message) {
-  this.timeOfCreation = new Date();
+var userName = '@bearabon';
+var fullName = "Scott Hendrickson";
+var avatarLocation = "img/alagoon.jpg";
+var Tweet = function(dateStamp, userName, fullName, avatarLocation, message) {
+  this.dateStamp = dateStamp;
   this.favoritedCount = 0;
   this.retweetedCount = 0;
   this.userName = userName;
@@ -188,7 +248,13 @@ Should I do it as the date stamp? That would keep everything unique.
 
 //* Implement the icons for when a tweet is favorited/retweeted in the upper right of the tweet.
 
-
+//$('.tweet-actions').find('.action-retweet').on('click', function() {
+var retweets = 0;
+$('.tweet-actions').find('.action-retweet').on('click', function() {
+        retweets++;
+        $('.tweet').find('num-retweets').val(retweets);
+        console.log(retweets);
+});
 
 
 
@@ -209,4 +275,7 @@ Should I do it as the date stamp? That would keep everything unique.
 
 //* Persist new tweets using a service like parse https://parse.com/
 
+});
+jQuery(document).ready(function() {
+  jQuery("time.timeago").timeago();
 });
